b3670cb90bcecb9f30262bb1adfb7e3b4f8bc2bc
Alessandro Cataldi <alessandro.cataldi@outlook.com>
2023-11-28 16:48:41 +0100

    feat: Added new proto.

---
+++ src/ExamVideoTest.tsx
@@ -0,0 +1,12 @@
+import Card from "./components/videointerview/Card";
+
+const ExamVideoTest = () => {
+  return (
+    <main id="videointerview">
+      <section id="layout">
+        <Card />
+      </section>
+    </main>
+  );
+};
+export default ExamVideoTest;
