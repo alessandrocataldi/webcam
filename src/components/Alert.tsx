44554cad5751721e0e804eb7c0296919eb4cbec0
Alessandro Cataldi <alessandro.cataldi@outlook.com>
2023-11-30 10:39:25 +0100

    feat: Added Alert component

---
+++ src/components/Alert.tsx
@@ -0,0 +1,12 @@
+const Alert = () => {
+  return (
+    <div
+      id="alert"
+      className="bg-red-600 text-cyan-50 text-center font-semibold text-base"
+    >
+      Connessione assente o lenta, non ricaricare la pagina.
+    </div>
+  );
+};
+
+export default Alert;
